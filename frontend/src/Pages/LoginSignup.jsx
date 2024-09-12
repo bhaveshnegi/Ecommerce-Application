f3c093c1ce17db1508a149bfd90e7926f5cb0834
Bhavesh Negi <bhaveshnegi98@gmail.com>
2024-09-12 19:13:46 +0530

    Made Login signup page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+        <h1>Sign Up</h1>
+        <div className="loginsignup-field">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Enter Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an Acccount? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By Continue i agree with terms of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
